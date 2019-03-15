b837c893da322d0e9a325207f3125ad495c66d42
Vitor Demenighi <vitordemenighi@10-41-40-25.0101.epoa.br.iosda.org>
2019-03-15 16:59:16 -0300

    contact between player and moster

--- Game.playground/Contents.swift
+++ Game.playground/Contents.swift
@@ -4,15 +4,37 @@ import PlaygroundSupport
 import SpriteKit
 
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    struct PhysicsCategory {
+        static let none      : UInt32 = 0
+        static let all       : UInt32 = UInt32.max
+        static let player   : UInt32 = 0b10       // 1
+        static let monster: UInt32 = 0b1      // 2
+    }
+    
     
     
     // 1
     let player = SKSpriteNode(imageNamed: "player")
     
+    
     override func didMove(to view: SKView) {
+        
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
+        
         // 2
         backgroundColor = SKColor.white
+        
+        
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2)
+        player.physicsBody?.isDynamic = true
+        player.physicsBody?.categoryBitMask = PhysicsCategory.player
+        player.physicsBody?.contactTestBitMask = PhysicsCategory.monster
+        player.physicsBody?.collisionBitMask = PhysicsCategory.none
+        player.physicsBody?.usesPreciseCollisionDetection = true
+        
         // 3
         player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.1)
         // 4
@@ -39,6 +61,12 @@ class GameScene: SKScene {
         // Create sprite
         let monster = SKSpriteNode(imageNamed: "monster")
         
+        monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size) // 1
+        monster.physicsBody?.isDynamic = true // 2
+        monster.physicsBody?.categoryBitMask = PhysicsCategory.monster // 3
+        monster.physicsBody?.contactTestBitMask = PhysicsCategory.player // 4
+        monster.physicsBody?.collisionBitMask = PhysicsCategory.none // 5
+        
         // Determine where to spawn the monster along the Y axis
         let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
         
@@ -50,7 +78,7 @@ class GameScene: SKScene {
         addChild(monster)
         
         // Determine speed of the monster
-        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        let actualDuration = random(min: CGFloat(1.0), max: CGFloat(3.0))
         
         // Create the actions
         let actionMove = SKAction.move(to: CGPoint(x: actualY, y: monster.size.height/2),
@@ -59,6 +87,8 @@ class GameScene: SKScene {
         monster.run(SKAction.sequence([actionMove, actionMoveDone]))
     }
     
+    
+    // Movimentacao do player
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         // 1 - Choose one of the touches to work with
         guard let touch = touches.first else {
@@ -66,13 +96,10 @@ class GameScene: SKScene {
         }
         let touchLocation = touch.location(in: self)
         
-        
-        print(touchLocation.x)
-        
-        
-        if touchLocation.x < 400 {
+        // ESQUERDA
+        if touchLocation.x < 250 {
             
-            var moveTo = player.position.x - 40
+            var moveTo = player.position.x - 50
             if moveTo < 0 || moveTo < 50 {
                 moveTo = 50
             }
@@ -80,11 +107,13 @@ class GameScene: SKScene {
             let actionMove = SKAction.move(to: CGPoint(x: moveTo, y: player.position.y),
                                            duration: TimeInterval(actualDuration))
             player.run(SKAction.sequence([actionMove]))
-        } else {
+        }
+        // DIREITA
+        else {
             
-            var moveTo = player.position.x + 40
-            if moveTo > 800 || moveTo > 750 {
-                moveTo = 750
+            var moveTo = player.position.x + 50
+            if moveTo > 500 || moveTo > 450 {
+                moveTo = 450
             }
             
             
@@ -93,6 +122,12 @@ class GameScene: SKScene {
                                            duration: TimeInterval(actualDuration))
             player.run(SKAction.sequence([actionMove]))
         }
+    }
+    
+    // ACAO QUANDO ENCOSTA O PLAYER
+    func projectileDidCollideWithMonster(player: SKSpriteNode, monster: SKSpriteNode) {
+        player.removeFromParent()
+        monster.removeFromParent()
     }
     
 
@@ -107,10 +142,34 @@ if let scene = GameScene(fileNamed: "GameScene") {
     
     // Present the scene
     sceneView.presentScene(scene)
-
-    
 }
 
+// IDENTIFICA O CONTATO
+extension GameScene {
+    func didBegin(_ contact: SKPhysicsContact) {
+        // 1
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+            print("Pegou")
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+            print("Nao sei")
+        }
+        
+        // 2
+        if ((firstBody.categoryBitMask & PhysicsCategory.monster != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.player != 0)) {
+            if let monster = firstBody.node as? SKSpriteNode,
+                let projectile = secondBody.node as? SKSpriteNode {
+                projectileDidCollideWithMonster(player: player, monster: monster)
+            }
+        }
+    }
+}
 
 
 PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
